bd1860df1f9e613bc04e7699e7c51ee4a18710b5
moon <slg419@nyu.edu>
2020-10-20 15:09:45 -0400

    made changes in feature branch

--- src/App.js
+++ src/App.js
@@ -16,8 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          learn react
-          just change
+         I just changed in feature branch!
         </a>
       </header>
     </div>
