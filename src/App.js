f5e09536607c5d6279a3cf887167726b311ff3dc
sam <slg419@nyu.edu>
2022-05-19 18:58:09 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello World!</h1>
+        <h2>This is modified</h2>
+
+      </div>
+    );
+  }
+}
+
+export default App;
